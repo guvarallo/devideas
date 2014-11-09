918778feafad7e8d811315010305173f10cd65f8
Gustavo Varallo <gustavo.varallo@live.com>
2014-11-09 20:58:29 -0300

    Added sessions and layout changes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+    
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome back #{user.username}!"
+      redirect_to root_path
+    else
+      flash[:warning] = "Oops! Invalid username or password, try again."
+      redirect_to login_path
+    end
+  end
+  
+  def destroy
+    flash[:notice] = "You've logged out!"
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'ideas#index'
 
+  get 'login',  to: 'sessions#new'
+  post 'login',  to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   resources :users, except: [:index]
   resources :ideas, except: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
