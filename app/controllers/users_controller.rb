c16536923fd7b547de8d20f58789c3f5fcb66797
Gustavo Varallo <gustavo.varallo@live.com>
2014-11-09 09:52:39 -0300

    Added controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  def show
-    
-  end
+  def show; end
 
   def new
     @user = User.new
@@ -20,19 +19,26 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    
-  end
+  def edit; end
 
   def update
-    
+    if @user.update
+      flash[:notice] = "User successfully edited."
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-    
+    @user.destroy
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
