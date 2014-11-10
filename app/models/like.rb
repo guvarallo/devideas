bcb194c58c69b7bdfe4787e4594c44b6bfbd4526
Gustavo Varallo <gustavo.varallo@live.com>
2014-11-10 20:47:08 -0300

    Corrected validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,6 @@ class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :idea
 
-  validates_uniqueness_of :user, scope: [:user_id]
+  validates_uniqueness_of :user, scope: [:idea_id]
 
 end
