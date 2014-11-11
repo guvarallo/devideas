a4ae1147ac5d6b83bb21b2f29fa2b2796bb7664f
Gustavo Varallo <gustavo.varallo@live.com>
2014-11-11 12:19:03 -0300

    Minor layout changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    flash[:notice] = "You've logged out!"
+    flash[:notice] = "See you soon #{user.username}!"
     session[:user_id] = nil
     redirect_to root_path
   end
